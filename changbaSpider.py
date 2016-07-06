e33276ab96d4b0d3a13a9ee935e6d60ef0120435
zhmhhu <zhmhhu@163.com>
2016-07-06 23:51:29 +0800

    使用API获取用户歌曲列表，参数为userid

--- changbaSpider.py
+++ changbaSpider.py
@@ -4,16 +4,79 @@ import urllib2
 import urllib  
 import re  
 import os  
-  
-#----------- 加载处理唱吧用户主页 -----------  
+import json
+import requests  
+#----------- 加载处理唱吧用户数据 -----------  
 class Spider_Model:  
       
     def __init__(self,userid):  
         self.uid = userid
-        self.List=[]  
+        self.List=[] 
+        
+        
+    def show_progress(self, response):
+        content = bytes()
+        total_size = response.headers.get('content-length')
+        if total_size is None:
+
+            content = response.content
+            return content
+        else:
+            total_size = int(total_size)
+            bytes_so_far = 0
+
+            for chunk in response.iter_content():
+                content += chunk
+                bytes_so_far += len(chunk)
+                progress = round(bytes_so_far * 1.0 / total_size * 100)
+                self.signal_load_progress.emit(progress)
+            return content
+
+    def http_request(self, method, action, query=None, urlencoded=None, callback=None, timeout=1):
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36"
+                          " (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36"
+        }
+        cookies = dict(appver="1.2.1", os="osx")
+        res = None
+        if method == "GET":
+            res = requests.get(action, headers=headers, cookies=cookies, timeout=timeout)
+        elif method == "POST":
+            res = requests.post(action, query, headers=self.headers, cookies=self.cookies, timeout=timeout)
+        elif method == "POST_UPDATE":
+            res = requests.post(action, query, headers=self.headers, cookies=self.cookies, timeout=timeout)
+            self.cookies.update(res.cookies.get_dict())
+            self.save_cookies()
+        content = self.show_progress(res)
+        content_str = content.decode('utf-8')
+        content_dict = json.loads(content_str)
+        return content_dict     
+  
+    def user_playlist(self, uid,pageNum):
+        action = 'http://changba.com/member/personcenter/loadmore.php?ver=1&type=0&curuserid=-1&pageNum='+str(pageNum)+'&userid='+uid
+#         action = 'http://changba.com/member/personcenter/loadmore.php?ver=1&type=0&curuserid=-1&pageNum=1&userid='+uid
+        res_data = self.http_request('GET', action)
+        return res_data
+    
+    def getNewlist(self,uid):
+        pageNo=0
+        playlists=[]
+        while(self.user_playlist(uid,pageNo) !=[]):
+            playlist = self.user_playlist(uid,pageNo)
+            for elem in playlist:
+                playlists.append(elem)
+            pageNo = pageNo+1
+            print playlists
+        items = [] 
+        for item in playlists:  
+            # item 中第一项是href中'/s/'后面的内容，也就是歌曲  
+            # item 中第二项是<a>标签之后，<div>标签之前的内容，也就是歌曲名称
+            print item  
+            items.append([item['enworkid'],item['songname']]) 
+        self.List = items
   
     # 将所有的作品都抠出来，添加到列表中并且返回列表  
-    def GetList(self,uid):  
+    '''def GetList(self,uid):  
         myUrl = "http://changba.com/u/" + uid  
         user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' 
         headers = { 'User-Agent' : user_agent } 
@@ -32,11 +95,15 @@ class Spider_Model:
             # item 中第一项是href中'/s/'后面的内容，也就是歌曲  
             # item 中第二项是<a>标签之后，<div>标签之前的内容，也就是歌曲名称  
             items.append([item[0].replace("\n",""),item[1].replace("\n","").replace("\t","").replace("\r","")]) 
-        self.List = items
+        self.List = items'''
   
+  
+    
           
     #根据mp3url查找MP3链接并下载     
     def Download(self,mp3url,mp3nm):
+        rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/\:*?"<>|'
+        mp3renm = re.sub(rstr, "", mp3nm) #剔除非法的文件名字符串
         myUrl = "http://changba.com/s/" + mp3url  
         user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' 
         headers = { 'User-Agent' : user_agent } 
@@ -53,7 +120,7 @@ class Spider_Model:
         if(myItems!=None):
             downurl=myItems.group()
             #文件存储在用户文件夹下
-            urllib.urlretrieve(downurl,os.path.expanduser('~/Downloads/%s.mp3' % mp3nm))
+            urllib.urlretrieve(downurl,os.path.expanduser('~/Downloads/%s.mp3' % mp3renm))
             print "mp3文件下载完成"
         else:
             print "未找到MP3文件" 
@@ -62,14 +129,15 @@ class Spider_Model:
   
         print u'正在下载请稍候......'  
           
-        self.GetList(self.uid)  
+        self.getNewlist(self.uid)  
         count = 1  
         for item in self.List:
             print u'正在下载第',count,"首歌"
             self.Download(item[0], item[1])
-            print item
+            #print item
             count+=1
 
+  
   
   
 #----------- 程序的入口处 -----------  
